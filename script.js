dd6867fb860ca5a2820928cee80a15ca40045e93
alzhanak <kenzhetayeva06@mail.ru>
2025-04-03 16:59:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// Получаем все ссылки панели
-навигации
+// Получаем все ссылки панели навигации
 const navLinks = document.querySelectorAll('.nav-link');
 // Получаем все разделы страницы
 const sections = document.querySelectorAll('.section');
 // Добавляем обработчик события для каждой ссылки
 navLinks.forEach(link => {
 link.addEventListener('click', () => {
-// Получаем идентификатор
-раздела, на который ссылается
-ссылка
+// Получаем идентификатор раздела, на который ссылается ссылка
 const targetId = link.getAttribute('href').substring(1);
-// Отображаем целевой раздел,
-скрывая все остальные
+// Отображаем целевой раздел, скрывая все остальные
 sections.forEach(section => {
 if (section.id === targetId) {
+  section.classList.add('active');
 } else {
-section.classList.add('active');
-section.classList.remove('active');
+  section.classList.remove('active');
 }
 });
 });
